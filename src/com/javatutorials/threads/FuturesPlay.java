4103ca3cdfd60b61d37a3a12154d645da750a781
VishalTambe <vishaltambe.m@gmail.com>
2024-07-26 14:12:06 +0530

    first commit

---
+++ src/com/javatutorials/threads/FuturesPlay.java
@@ -0,0 +1,42 @@
+package com.javatutorials.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class FuturesPlay {
+
+	public static Integer doSimpleTask() {
+		System.out.println("Start of Thread: " + Thread.currentThread().getName());
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+			System.out.println("Task interrupted");
+		}
+		System.out.println("End of Thread: " + Thread.currentThread().getName());
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		ExecutorService service = null;
+		try {
+			service = Executors.newSingleThreadExecutor();
+			Future<?> future = service.submit(FuturesPlay::doSimpleTask);
+			getName();
+			System.out.println(future.get());
+		} catch (Exception e) {
+			System.out.println("Error in main");
+		} finally {
+			if (service != null)
+				service.shutdown();
+
+		}
+	}
+
+	private static void getName() {
+		System.out.println("My Name is vishal and I am responding in between thread executorService execution");
+
+	}
+}
